e8d1403a61b97f877a73cbe12ea2ea1e7eada9ee
Shitij Aggarwal <156359542+PerkyNeptune7@users.noreply.github.com>
2024-10-12 19:26:12 -0400

    Added project files

---
+++ env/pylupdate5.bat
@@ -0,0 +1,1 @@
+@C:/Users/shiti/Desktop/ML-AI course/sample_project1/env\python.exe -m PyQt5.pylupdate_main %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ env/pyuic5.bat
@@ -0,0 +1,1 @@
+@C:/Users/shiti/Desktop/ML-AI course/sample_project1/env\python.exe -m PyQt5.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
